222ef7e5d13c3b084294db31ad89e2910a755018
lipengfei <1401446942@qq.com>
2022-06-07 22:13:06 +0800

    style:增加上下键切换item、esc取消搜索框、enter确认结果

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,6 +1,15 @@
-import React, { ChangeEvent, FC, useState, ReactElement } from "react";
+import React, {
+  ChangeEvent,
+  FC,
+  useState,
+  ReactElement,
+  KeyboardEvent,
+  useRef,
+  useEffect,
+} from "react";
 import Input, { InputProps } from "../Input/Input";
 import classNames from "classnames";
+import useClickOutside from "../../hooks/useClickOutside";
 
 export type AutoCompleteProps = {
   fetchSuggestions: (str: string) => string[];
@@ -15,8 +24,15 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
     renderOption,
     ...reset
   } = props;
+
   const [inputValue, setInputValue] = useState<string>("");
   const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [highlightIndex, setHighlightIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  //   useEffect(() => {
+  //     console.log(123);
+  //   }, []);
+  useClickOutside(containerRef, () => setSuggestions([]));
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInputValue(value);
@@ -38,16 +54,54 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const renderTemplate = (item: string) => {
     return renderOption ? renderOption(item) : item;
   };
+  const highlight = (index: number) => {
+    if (index < 0) index = 0;
+    if (index >= suggestions.length) index = suggestions.length - 1;
+    setHighlightIndex(index);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      case 13:
+        setInputValue(suggestions[highlightIndex]); //回车
+        setSuggestions([]);
+        break;
+      case 38:
+        highlight(highlightIndex - 1); //向上
+        break;
+      case 40:
+        highlight(highlightIndex + 1); //向下
+        break;
+      case 27:
+        setSuggestions([]); //取消
+        break;
+      default:
+        break;
+    }
+  };
   return (
-    <div className="pf-auto-complete">
-      <Input onChange={handleChange} defaultValue={inputValue} {...reset} />
+    <div className="pf-auto-complete" ref={containerRef}>
+      <Input
+        onChange={handleChange}
+        defaultValue={inputValue}
+        onKeyDown={handleKeyDown}
+        {...reset}
+      />
       {suggestions.length > 0 && (
         <ul>
-          {suggestions.map((item, index) => (
-            <li key={index} onClick={() => handleSelect(item)}>
-              {renderTemplate(item)}
-            </li>
-          ))}
+          {suggestions.map((item, index) => {
+            const classNamess = classNames("suggesItem", {
+              suggesItemActive: index === highlightIndex,
+            });
+            return (
+              <li
+                key={index}
+                className={classNamess}
+                onClick={() => handleSelect(item)}
+              >
+                {renderTemplate(item)}
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+export default function useDebounce(fun: any, time = 300) {
+  const [debounceVal, setDebounceVal] = useState(fun);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceVal(fun);
+    }, time);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [fun, time]);
+  return debounceVal;
+}
